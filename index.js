6557efc5de9297f30f9d3024102cca65eafdbbfa
Jacob Parris <typefortyproductions@gmail.com>
2024-07-20 17:19:41 -0400

    Added Startbutton and second window

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
+// Declare mainWindow and controlWindow at a higher scope
+let mainWindow;
+let controlWindow;
 
-function createWindow() {
-    const mainWindow = new BrowserWindow({
+function createDisplayWindow() {
+    mainWindow = new BrowserWindow({
         width: Math.round(500),
         height: 1000,
         backgroundColor: '#000000',
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
 
     mainWindow.loadFile('index.html');
 
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', () => {
         mainWindow = null;
     });
 }
 
-app.on('ready', createWindow);
+function createControllerWindow() {
+    controlWindow = new BrowserWindow({
+        width: 500,
+        height: 500,
+        backgroundColor: '#000000',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
-app.on('window-all-closed', function () {
+    controlWindow.loadFile('controller.html');
+
+    controlWindow.on('closed', () => {
+        controlWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createDisplayWindow();
+    createControllerWindow();
+});
+
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
+app.on('activate', () => {
+    if (!mainWindow) {
+        createDisplayWindow();
+    }
+    if (!controlWindow) {
+        createControllerWindow();
+    }
+});
+
+ipcMain.on('start-timer', (event, data) => {
+    // Now mainWindow is correctly referenced
+    if (mainWindow) {
+        mainWindow.webContents.send('start-timer', data);
     }
 });
